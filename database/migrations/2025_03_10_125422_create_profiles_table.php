5122804e4a78a95e96cf7c231938744a9eff4f7a
sandro Pereira <smpsandro1239@gmail.com>
2025-03-10 12:57:17 +0000

    Configuração do banco de dados: tabelas users e profiles

---
+++ database/migrations/2025_03_10_125422_create_profiles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->string('full_name')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('resume_file_path')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
